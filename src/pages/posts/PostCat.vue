a5f1e00d00be8e5f11176c78ff55794179f3461d
Ares Ioakimidis <arisiwak@gmail.com>
2023-07-18 18:02:22 +0300

    removed-postfilters

--- src/pages/posts/PostCat.vue
+++ src/pages/posts/PostCat.vue
@@ -2,34 +2,17 @@
   <base-container>
     <div class="latest-posts_container">
       Post from {{ category }}
-      <ul v-if="hasPosts">
-        <post-item v-for="post in filteredPosts"
-          :key="post.id"
-          :id="post.id"
-          :title="post.title"
-          :category="post.category"
-          :excerpt="post.excerpt"
-          :postImg="post.postImg"
-        >
-        </post-item>
-      </ul>
-    <h2 v-else>No posts yet</h2>
-    
     </div>
   </base-container>
  </template>
 
 <script> 
-import PostItem from '../../components/posts/PostItem.vue';
 
-export default {
-  components: {
-    PostItem,
-  },
+
+export default {  
   props: ['id','category'],
   data() {
     return {
-      
     };
   },
   computed: {
