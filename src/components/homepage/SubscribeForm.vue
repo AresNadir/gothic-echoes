02045275d47c6584ddb6c0786801a7d9cda0a645
Ares Ioakimidis <arisiwak@gmail.com>
2023-07-19 11:56:00 +0300

    install axios

--- src/components/homepage/SubscribeForm.vue
+++ src/components/homepage/SubscribeForm.vue
@@ -1,3 +1,20 @@
 <template>
-  Subscribe to our newsletter
+  <div>
+    <h2>Subscribe to newsletter</h2>
+    <form @submit.prevent="submitForm">
+      <label for="email">Email</label>
+      <input type="email">
+      <button>Submit</button>
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    submitForm() {
+      console.log('form submited');
+    }
+  }
+}
+</script>
