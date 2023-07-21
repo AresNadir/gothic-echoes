7f78850288a356307dc86cdcc7fa48c363d00fc9
Ares Ioakimidis <arisiwak@gmail.com>
2023-07-21 15:36:45 +0300

    added to the content store

--- src/store/modules/content/index.js
+++ src/store/modules/content/index.js
@@ -1,3 +1,21 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
 export default {
+  state() {
+    return {
+      cont:[
+        {
+          id:'cont1',
+          content:'lorem ipsum',
+          pageLocation:'test-demo',
+        }
+      ],
+    };
+  },
+  mutations,
+  actions,
+  getters,
 
 };
